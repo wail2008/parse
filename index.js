b03779596626a208e4444c62b3d5b4a89eacc409
Wael <wael@Waels-MacBook-Air.local>
2017-01-30 03:52:23 -0800

    make it better

--- index.js
+++ index.js
@@ -36,16 +36,15 @@ if (accessKeyId && secretAccessKey && bucketName) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://heroku_1dn9w5bn:212uo0mukqcm8b86kc8oqc1um7@ds017886.mlab.com:17886/heroku_1dn9w5bn',
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: scmswael_schoolz,
-  masterKey: 8559165053,
-  serverURL: https://schools-cms.herokuapp.com/,
-  restAPIKey: restAPIKey,
-javascriptKey: process.env.JAVASCRIPT_KEY || '6243885914', 
+  appId: appId,
+  masterKey: masterKey,
+  serverURL: serverUrl,
+  restAPIKey: restApiKey,
   filesAdapter: filesAdapter,
   verifyUserEmails: false,
-  publicServerURL: https://schools-cms.herokuapp.com/,
+  publicServerURL: publicServerUrl,
   appName: appName,
   emailAdapter: {
     module: 'parse-server-simple-mailgun-adapter',
